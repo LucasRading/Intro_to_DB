2c357bfe1b809471cc077ee1cbe40f3feca32451
Lucas Rading <126816656+LucasRading@users.noreply.github.com>
2025-07-26 20:13:23 +0300

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,10 +1,2 @@
-import mysql.connector
-import json
 
-def db_connection():
-    conn= mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="(@5982LUCAS)doreen",
-        database="alx_book_store"
         
